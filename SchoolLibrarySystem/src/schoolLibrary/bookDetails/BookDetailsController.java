34a1ad7417f71870c42685d1c88e3a4160651c3e
CHico-Lee <leechiho22@hotmail.com>
2017-05-02 15:03:24 -0400

    display book rating, added finally block to disconnect DB to aviod SQLite_BUSY Error, Added comments,

--- SchoolLibrarySystem/src/schoolLibrary/bookDetails/BookDetailsController.java
+++ SchoolLibrarySystem/src/schoolLibrary/bookDetails/BookDetailsController.java
@@ -1,3 +1,10 @@
+/**
+* COMP 580 Final Project (Spring 2017)
+* Controller class to displays detail book information after user selected a book.
+*
+* @author  Chi Ho Lee, Bijay Maharjan
+* @since   2017-4-26
+*/
 package schoolLibrary.bookDetails;
 
 import java.io.IOException;
@@ -6,10 +13,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -19,11 +22,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Alert.AlertType;
 import schoolLibrary.Main;
 import schoolLibrary.borrowBook.BorrowBookController;
-import schoolLibrary.searchBook.Row;
 
 public class BookDetailsController {
-
-	private Connection connection;
 	
 	@FXML
 	private Label isbnLbl;
@@ -39,85 +39,96 @@ public class BookDetailsController {
 	private Label desLbl;
 	@FXML
 	private Label catLbl;
+	@FXML
+	private Label ratingLbl;
 	
 	@FXML
 	private Button selectBtn;
 	
+	private Connection connection;
 	private Main main;
 	
-	
+	// variable to store Book ISBN going to display.
 	private static String selectedIsbn = null;
-	
+	/**
+	 * This is a static method accept the ISBN before initialize the scene.
+	 * @param  isbn  Book ISBN in String format.
+	 */
 	public static void setIsbn(String isbn) {
 		selectedIsbn = isbn;
 	}
 	
-	 
-	    @FXML
-	    private void initialize() throws SQLException {
-	    	// load the sqlite-JDBC driver using the current class loader
-	    	try {
-	    		Class.forName( "org.sqlite.JDBC" );
-	    	} catch (ClassNotFoundException e1) {
-	    	};
-	    	
-	        // connect to the database
-	        try {
-				connection = DriverManager.getConnection( "jdbc:sqlite:SchoolLibrarySystem.sqlite");
-			} catch (SQLException e) {
-				handleError(e);
+    @FXML
+    private void initialize() throws SQLException {
+    	// load the sqlite-JDBC driver using the current class loader
+    	try {
+    		Class.forName( "org.sqlite.JDBC" );
+    	} catch (ClassNotFoundException e1) {
+    	}
+    	
+        // connect to the database
+        try {
+			connection = DriverManager.getConnection( "jdbc:sqlite:SchoolLibrarySystem.sqlite");
+			
+	        // generate parameterized sql to get a single book information, also calculate avg of the rating.
+			String sql = "SELECT Book.ISBN AS book_isbn, Book.Title AS book_title, Book.Author AS book_author," + 
+							" Book.Publisher AS book_pub, Book.Year AS book_year, Book.Description AS book_des," + 
+							" (Book.CategoryId || ' - ' || Category.CategoryName) AS book_cat," +
+							" ifnull(ROUND(AVG(Borrow.Rating), 2), 'Not rated') AS rating"+
+							" FROM Book" + 
+							" JOIN Category USING (CategoryId)" +
+							" LEFT JOIN Borrow USING (ISBN)" +
+							" WHERE Book.ISBN = ?;";
+				
+			// prepared statement
+			PreparedStatement stmt= connection.prepareStatement( sql );
+			stmt.setString( 1, selectedIsbn);
+						
+			// get results
+			ResultSet res = stmt.executeQuery();
+			
+			// put data into label.
+			isbnLbl.setText(selectedIsbn);
+			
+			// Update corresponding labels from result.
+			if ( res.next() ) {
+				titleLbl.setText(res.getString("book_title"));
+				authorLbl.setText(res.getString("book_author"));
+				pubLbl.setText(res.getString("book_pub"));
+				yearLbl.setText(res.getString("book_year"));
+				desLbl.setText(res.getString("book_des"));
+				catLbl.setText(res.getString("book_cat"));
+				ratingLbl.setText(res.getString("rating"));
 			}
-	        
-	        // generate parameterized sql
- 			String sql = "SELECT Book.ISBN AS book_isbn, Book.Title AS book_title, Book.Author AS book_author," + 
- 							" Book.Publisher AS book_pub, Book.Year AS book_year, Book.Description AS book_des," + 
- 							" (Book.CategoryId || ' - ' || CATEGORY.CategoryName) AS book_cat" +
- 							" FROM BOOK" + 
- 							" JOIN CATEGORY USING (CategoryId)" +
- 							" WHERE Book.ISBN = ?;";
- 				
- 			// prepared statement
- 			PreparedStatement stmt= connection.prepareStatement( sql );
- 						
- 			stmt.setString( 1, selectedIsbn);
- 						
- 			// get results
- 			ResultSet res = stmt.executeQuery();
- 			
- 			isbnLbl.setText(selectedIsbn);
- 			
- 			while ( res.next() ) {
- 				titleLbl.setText(res.getString("book_title"));
- 				authorLbl.setText(res.getString("book_author"));
- 				pubLbl.setText(res.getString("book_pub"));
- 				yearLbl.setText(res.getString("book_year"));
- 				desLbl.setText(res.getString("book_des"));
- 				catLbl.setText(res.getString("book_cat"));
- 			}
-	    }
+        } catch (SQLException e) {
+			handleError(e);
+		} finally {
+			if (connection != null) {
+				try {
+		    	// Avoid SQLite_BUSY Error - database is locked
+					connection.close();
+				} catch (SQLException e) {
+					handleError(e);
+				}
+			}
+		} 
+    }	// end of initialize()
+
+	private void handleError(Exception e) {
+		// Alert the user when things go terribly wrong
+		Alert alert = new Alert(AlertType.ERROR, e.getMessage(), ButtonType.CLOSE);
+		// Close the program when the user clicks close on the alert
+		alert.setOnCloseRequest(event -> Platform.exit());
+		// show the alert
+		alert.show();
+	}
 	
-		private void handleError(Exception e) {
-			// Alert the user when things go terribly wrong
-			Alert alert = new Alert(AlertType.ERROR, e.getMessage(), ButtonType.CLOSE);
-			// Close the program when the user clicks close on the alert
-			alert.setOnCloseRequest(event -> Platform.exit());
-			// show the alert
-			alert.show();
-		}
-		
-		@FXML
-		private void clickSelect() throws IOException {
-			try {
-			BorrowBookController.setIsbn(selectedIsbn);
-			main.showBorrowBookScene();
-			}
-			catch (NullPointerException e) {
-				// Alert the user when things go terribly wrong
-				Alert alert = new Alert(AlertType.ERROR, e.getMessage(), ButtonType.CLOSE);
-				alert.setHeaderText("Please select a book.");
-				// show the alert
-				alert.show();
-			}
-		}
+	// Method to handle button click to Borrow this book.
+	@FXML
+	private void clickSelect() throws IOException {
+		// Passing ISBN to borrow book controller. 
+		BorrowBookController.setIsbn(selectedIsbn);
+		main.showBorrowBookScene();
+	}
 
 }
